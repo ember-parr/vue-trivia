a1f4a5cd795de3d23142269d4668928a6540b676
Ember Parr <ember-devops@gmail.com>
2021-03-08 13:56:57 -0600

    added trivia data

---
+++ src/assets/trivia.js
@@ -0,0 +1,126 @@
+export default [
+  {
+    question:
+      "In Florida, only on Sundays, it is illegal for a single woman to do... what?",
+    answer: "Skydive",
+    answerShown: false,
+    difficulty: "hard"
+  },
+  {
+    question: "Johnny Depp is famously afraid of what?",
+    answer: "Clowns",
+    answerShown: false,
+    difficulty: "hard"
+  },
+  {
+    question:
+      "In California, you can't legally buy a mousetrap without having what?",
+    answer: "A hunting license",
+    answerShown: false,
+    difficulty: "hard"
+  },
+  {
+    question: "It is illegal to do what in the French vineyards?",
+    answer: "Land a flying saucer",
+    answerShown: false,
+    difficulty: "hard"
+  },
+  {
+    question: "Coprastastaphobia is the fear of what?",
+    answer: "Constipation",
+    answerShown: false,
+    difficulty: "easy"
+  },
+  {
+    question: "In Texas, it’s illegal to swear in front of what?",
+    answer: "A corpse",
+    answerShown: false,
+    difficulty: "hard"
+  },
+  {
+    question: "In Georgia, it’s illegal to eat what with a fork?",
+    answer: "Fried Chicken",
+    answerShown: false,
+    difficulty: "hard"
+  },
+  {
+    question:
+      "The first hockey pucks used in early outdoor hockey games were made of what?",
+    answer: "Frozen cow dung (yes, that is poop)",
+    answerShown: false,
+    difficulty: "hard"
+  },
+  {
+    question: "In Minnesota, it is illegal to tease what type of animal?",
+    answer: "Skunks",
+    answerShown: false,
+    difficulty: "hard"
+  },
+  {
+    question: "How many teeth does an aardvark have?",
+    answer: "Zero",
+    answerShown: false,
+    difficulty: "hard"
+  },
+  {
+    question: "What was Marilyn Monroe’s natural hair color?",
+    answer: "Red",
+    answerShown: false,
+    difficulty: "hard"
+  },
+  {
+    question: "Who sang about being an 'eggman' and a 'walrus'?",
+    answer: "The Beatles",
+    answerShown: false,
+    difficulty: "hard"
+  },
+  {
+    question: "Which Tasmanian marsupial is known for its temper?",
+    answer: "Tasmanian Devil",
+    answerShown: false,
+    difficulty: "easy"
+  },
+  {
+    question: "Where on the human body is the zygomatic bone found?",
+    answer: "Facial Cheek",
+    answerShown: false,
+    difficulty: "hard"
+  },
+  {
+    question: "It was illegal for women to wear what in 19th century Florence?",
+    answer: "Buttons",
+    answerShown: false,
+    difficulty: "hard"
+  },
+  {
+    question: "Who claimed he could “drive away the devil with a fart?”",
+    answer: "Martin Luther",
+    answerShown: false,
+    difficulty: "hard"
+  },
+  {
+    question:
+      "In St. Louis, Missouri, it’s illegal for a firefighter to rescue who?",
+    answer: "Undressed women (or women in a nightgown)",
+    answerShown: false,
+    difficulty: "hard"
+  },
+  {
+    question: "What was banned in Indonesia for 'stimulating passion'? ",
+    answer: "Hula hoops",
+    answerShown: false,
+    difficulty: "hard"
+  },
+  {
+    question: "What was Hugh Hefner’s jet plane named?",
+    answer: "Big Bunny",
+    answerShown: false,
+    difficulty: "hard"
+  },
+  {
+    question: "In June in Wyoming, it is illegal to take a picture of what?",
+    answer: "A rabbit",
+    answerShown: false,
+    difficulty: "hard"
+  }
+];
